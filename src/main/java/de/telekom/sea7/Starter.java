8527797dfcdeb4d4d354672e1d6e2a5c9bc20a3e
Felix Meurer <f.meurer@telekom.de>
2022-01-13 11:49:17 +0100

    Initial creation

---
+++ src/main/java/de/telekom/sea7/Starter.java
@@ -0,0 +1,13 @@
+package de.telekom.sea7;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Application application = new Application();
+		application.run();
+
+	}
+
+}
